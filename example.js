c8d503df9fbb5302938809a5c3ee59de1e58e75a
Jacob Schulzetenberg <jacob.schulzetenberg@gmail.com>
2019-09-29 19:59:23 -0600

    Render server-side using HTML templating

---
+++ example.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    name: 'Marshmello',
+    image: 'https://i.scdn.co/image/719dab7f27ba0793b4ca7c5672ecf56d3d88467b'
+  },
+  {
+    name: 'We Came As Romans',
+    image: 'http://img2-ak.lst.fm/i/u/174s/eb5236205e1e488899bdc52b7090476f.png'
+  },
+  {
+    name: 'The Chainsmokers',
+    image: 'https://i.scdn.co/image/9da714082fe9696529abadc8e4095451221b4483'
+  },
+  {
+    name: 'Hillsong United',
+    image: 'https://i.scdn.co/image/5b6a5e16df100141e93bf8fb695042d375e96e77'
+  },
+  {
+    name: 'A Day to Remember',
+    image: 'http://img2-ak.lst.fm/i/u/174s/1fe6c478ff994186b40bcecd1468993a.png'
+  },
+  {
+    name: 'NF',
+    image: 'http://img2-ak.lst.fm/i/u/174s/cc6304fbf8c1d6a39bfad62e660b6b99.png'
+  },
+  {
+    name: 'For All Those Sleeping',
+    image: 'http://img2-ak.lst.fm/i/u/174s/81179966b2804e8b8832a505553afe68.png'
+  },
+  {
+    name: 'One Direction',
+    image: 'http://img2-ak.lst.fm/i/u/770x0/5679d6de42f7375d2118db756a87d8fd.jpg'
+  },
+  {
+    name: 'Odesa',
+    image: 'https://i.scdn.co/image/881192d40935946e36d534e24a3df821579c5b83'
+  },
+];
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const path = require('path');
+const fs = require('fs');
+const ejs = require('ejs');
 const puppeteer = require('puppeteer');
+
+const artistData = require('./example');
 
 const viewPort = { width: 1280, height: 600 };
 const options = {
@@ -15,11 +19,30 @@ const options = {
   */
 };
 
+const templatePage = fs.readFileSync(`${path.join(__dirname, 'main.ejs')}`, 'utf8');
+const stylesCss = fs.readFileSync(`${path.join(__dirname, 'css/styles.css')}`, 'utf8');
+const normalizeCss = fs.readFileSync(`${path.join(__dirname, 'css/normalize.css')}`, 'utf8');
+const scatteredPolaroids = fs.readFileSync(`${path.join(__dirname, 'js/scattered-polaroids.js')}`, 'utf8');
+
+const templateData = {
+  stylesCss,
+  normalizeCss,
+  scatteredPolaroids,
+  artistData
+};
+
+if(artistData.length === 0) {
+  return console.error('No artist data!');
+}
+
+const renderedPage = ejs.render(templatePage, templateData, {});
+
 (async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.setViewport(viewPort)
-  await page.goto(`${path.join(__dirname, 'index.html')}`, { waitUntil: 'networkidle0'} );
-  await page.screenshot(options)
-  await browser.close()
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport(viewPort);
+  console.log(renderedPage);
+  await page.setContent(renderedPage, { waitUntil: 'networkidle0'} );
+  await page.screenshot(options);
+  await browser.close();
 })()
