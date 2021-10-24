52bd0ce5b4d5072e546f2ffa71e2fd7a0ff1c698
Jacob <jacob.schulzetenberg@gmail.com>
2021-10-24 18:19:26 -0500

    Update example data

--- example.js
+++ example.js
@@ -1,38 +1,64 @@
 module.exports = [
-  {
-    name: 'Marshmello',
-    image: 'https://i.scdn.co/image/719dab7f27ba0793b4ca7c5672ecf56d3d88467b'
-  },
-  {
-    name: 'We Came As Romans',
-    image: 'http://img2-ak.lst.fm/i/u/174s/eb5236205e1e488899bdc52b7090476f.png'
-  },
-  {
-    name: 'The Chainsmokers',
-    image: 'https://i.scdn.co/image/9da714082fe9696529abadc8e4095451221b4483'
-  },
-  {
-    name: 'Hillsong United',
-    image: 'https://i.scdn.co/image/5b6a5e16df100141e93bf8fb695042d375e96e77'
-  },
-  {
-    name: 'A Day to Remember',
-    image: 'http://img2-ak.lst.fm/i/u/174s/1fe6c478ff994186b40bcecd1468993a.png'
-  },
-  {
-    name: 'NF',
-    image: 'http://img2-ak.lst.fm/i/u/174s/cc6304fbf8c1d6a39bfad62e660b6b99.png'
-  },
-  {
-    name: 'For All Those Sleeping',
-    image: 'http://img2-ak.lst.fm/i/u/174s/81179966b2804e8b8832a505553afe68.png'
-  },
-  {
-    name: 'One Direction',
-    image: 'http://img2-ak.lst.fm/i/u/770x0/5679d6de42f7375d2118db756a87d8fd.jpg'
-  },
-  {
-    name: 'Odesa',
-    image: 'https://i.scdn.co/image/881192d40935946e36d534e24a3df821579c5b83'
-  },
+  [
+    {
+        name: 'Quinn XCII',
+        image: 'https://res.cloudinary.com/schulzetenberg/image/upload/v1633478407/music/quinn-xcii-artist.jpg'
+    },
+    {
+        name: 'Cheat Codes',
+        image: 'https://res.cloudinary.com/schulzetenberg/image/upload/v1624924807/music/cheat-codes-artist.jpg'
+    },
+    {
+        name: 'Boy In Space',
+        image: 'https://res.cloudinary.com/schulzetenberg/image/upload/v1632614407/music/boy-in-space-artist.jpg'
+    },
+    {
+        name: 'Our Last Night',
+        image: 'https://res.cloudinary.com/schulzetenberg/image/upload/v1625443207/music/our-last-night-artist.jpg'
+    },
+    {
+        name: 'Georgia Ku',
+        image: 'https://res.cloudinary.com/schulzetenberg/image/upload/v1630281607/music/georgia-ku-artist.jpg'
+    },
+    {
+        name: 'Chelsea Cutler',
+        image: 'https://res.cloudinary.com/schulzetenberg/image/upload/v1633305607/music/chelsea-cutler-artist.jpg'
+    },
+    {
+        name: 'Taylor Swift',
+        image: 'https://res.cloudinary.com/schulzetenberg/image/upload/v1625356807/music/taylor-swift-artist.jpg'
+    },
+    {
+        name: 'Clara Mae',
+        image: 'https://res.cloudinary.com/schulzetenberg/image/upload/v1631491207/music/clara-mae-artist.jpg'
+    },
+    {
+        name: 'The Chainsmokers',
+        image: 'https://res.cloudinary.com/schulzetenberg/image/upload/v1625443207/music/the-chainsmokers-artist.jpg'
+    },
+    {
+        name: 'Justin Bieber',
+        image: 'https://res.cloudinary.com/schulzetenberg/image/upload/v1625443207/music/justin-bieber-artist.jpg'
+    },
+    {
+        name: 'Kygo',
+        image: 'https://res.cloudinary.com/schulzetenberg/image/upload/v1625356807/music/kygo-artist.jpg'
+    },
+    {
+        name: 'SHY Martin',
+        image: 'https://res.cloudinary.com/schulzetenberg/image/upload/v1632096007/music/shy-martin-artist.jpg'
+    },
+    {
+        name: 'Julia Michaels',
+        image: 'https://res.cloudinary.com/schulzetenberg/image/upload/v1631750407/music/julia-michaels-artist.jpg'
+    },
+    {
+        name: 'Lost Kings',
+        image: 'https://res.cloudinary.com/schulzetenberg/image/upload/v1622332807/music/lost-kings-artist.jpg'
+    },
+    {
+        name: 'NOTD',
+        image: 'https://res.cloudinary.com/schulzetenberg/image/upload/v1625356807/music/notd-artist.jpg'
+    }
+]
 ];
